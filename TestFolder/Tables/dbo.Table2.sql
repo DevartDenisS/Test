baeef863f22a8a9b5e87f6920fa834f86fc2f143
DevartDenisS <DevartDenisS>
2024-04-27 10:57:51 +0300

    Committed using Toad Data Studio Source Control

---
+++ TestFolder/Tables/dbo.Table2.sql
@@ -0,0 +1,7 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+
+CREATE TABLE [dbo].[Table2] (
+	[col1] int,
+	[col2] char(50) COLLATE Cyrillic_General_CI_AS
+) ON [PRIMARY]
---
+++ TestFolder/Tables/dbo.Test.sql
@@ -0,0 +1,6 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+
+CREATE TABLE [dbo].[Test] (
+	[Column_2] int
+) ON [PRIMARY]
