481ff92a11c6af71c415361da5f993be071622fa
DevartDenisS <DevartDenisS>
2024-04-27 10:59:22 +0300

    Committed using Toad Data Studio Source Control

--- TestFolder/Tables/dbo.actor.sql
+++ TestFolder/Tables/dbo.actor.sql
@@ -3,7 +3,7 @@ SET QUOTED_IDENTIFIER ON
 
 CREATE TABLE [dbo].[actor] (
 	[actor_id] int IDENTITY(1, 1),
-	[first_name] varchar(45) COLLATE Cyrillic_General_CI_AS NOT NULL,
+	[first_name] varchar(74) COLLATE Cyrillic_General_CI_AS NOT NULL,
 	[last_name] varchar(45) COLLATE Cyrillic_General_CI_AS NOT NULL,
 	[last_update] datetime NOT NULL CONSTRAINT [DF_actor_last_update] DEFAULT (getdate()),
 	PRIMARY KEY NONCLUSTERED([actor_id]) WITH (FILLFACTOR=100) ON [PRIMARY]
