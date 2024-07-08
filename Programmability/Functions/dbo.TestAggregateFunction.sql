70a1d4ff5200d17b6890613f1590e47d3f3b52f1
DevartDenisS <132042766+DevartDenisS@users.noreply.github.com>
2024-07-08 16:52:52 +0300

    Delete Programmability directory

--- Programmability/Functions/dbo.TestAggregateFunction.sql
+++
@@ -1,6 +0,0 @@
-﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
-GO
-CREATE AGGREGATE [dbo].[TestAggregateFunction] (@input [nvarchar](200))
-RETURNS [nvarchar](max)
-EXTERNAL NAME [SqlClassLibrary].[Aggregate1]
-GO
--- Programmability/Functions/dbo.clrScalarFunction1.sql
+++
@@ -1,7 +0,0 @@
-﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
-GO
-CREATE FUNCTION [dbo].[clrScalarFunction1] (@f [float])
-RETURNS [float]
-AS
-EXTERNAL NAME [Clr_Assembly1].[Clr_function.APress.Samples.Sql].[Fahrenheit2Celsius]
-GO
