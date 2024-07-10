813d42d905f81c4fefe6d34bd7333905eac34f58
DevartDenisS <Deniss@devart.com>
2024-07-10 11:30:36 +0300

    First commit

---
+++ Data/dbo.TestTable_Data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[TestTable] ([Col1], [Col2]) VALUES (1, N'String1             ')
+INSERT INTO [dbo].[TestTable] ([Col1], [Col2]) VALUES (2, N'String2             ')
+INSERT INTO [dbo].[TestTable] ([Col1], [Col2]) VALUES (3, N'String3             ')
