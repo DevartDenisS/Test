d471ed855ff56cc796fe88689e1d6f6b4f79712c
DenisS <deniss@devart.com>
2026-08-06 11:12:24 +0300

    2

---
+++ Tables/StaticData/employees.Data.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO employees(id, first_name, last_name, salary, created_at, updated_at) VALUES
+(1, 'John', 'Smith', 55000.00, '2026-08-06 10:51:56', '2026-08-06 10:51:56');
+INSERT INTO employees(id, first_name, last_name, salary, created_at, updated_at) VALUES
+(2, 'Emma', 'Johnson', 62000.00, '2026-08-06 10:51:56', '2026-08-06 10:51:56');
+INSERT INTO employees(id, first_name, last_name, salary, created_at, updated_at) VALUES
+(3, 'Michael', 'Brown', 71000.00, '2026-08-06 10:51:56', '2026-08-06 10:51:56');
+INSERT INTO employees(id, first_name, last_name, salary, created_at, updated_at) VALUES
+(4, 'Olivia', 'Davis', 68000.00, '2026-08-06 10:51:56', '2026-08-06 10:51:56');
+INSERT INTO employees(id, first_name, last_name, salary, created_at, updated_at) VALUES
+(5, 'William', 'Wilson', 59000.00, '2026-08-06 10:51:56', '2026-08-06 10:51:56');
